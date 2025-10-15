894b39c249f8cf06530ca0faeeb240e57065ebbb
Tomone-Nomura <tomonenomura@gmail.com>
2025-10-16 06:23:48 +0900

    Enable PWA: Service Worker registration and manifest.json update

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,3 +13,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// PWAとして動作させるためService Workerを登録
+serviceWorkerRegistration.register();
